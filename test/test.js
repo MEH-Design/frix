98f51ff6c5831b86703655cfdc9dee0499850c54
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2016-08-13 20:55:03 +0200

    set eslint env to mocha, renamed some thing to frix, fixed frix module names

--- test/test.js
+++ test/test.js
@@ -1,47 +1,46 @@
 'use strict';
 
 const chai = require('chai');
-chai.use(require('chai-http'));
 const expect = chai.expect;
 const proxyquire = require('proxyquire');
-const atomicms = proxyquire('../lib/atomicms', {
-  'app-root-path': { path: 'test/files' }
-});
-const fs = require('fs');
 const keva = require('keva');
-const express = require('express');
+const frix = proxyquire('../lib/frix', {
+  'app-root-path': {
+    path: 'test/files'
+  }
+});
 
+chai.use(require('chai-http'));
 
-describe('atomicms', function() {
-  let cms = atomicms;
-  cms.render();
+describe('frix', function() {
+  frix.render();
+
   describe('modules', function() {
-
     let someFunction = function(html) {
       return html;
     };
 
     it('should be added', function() {
-      cms.addModule('html-content', someFunction);
-      expect(cms.modules['html-content'].includes(someFunction))
+      frix.addModule('content', someFunction);
+      expect(frix.modules['content'].includes(someFunction))
         .to.be.true;
     });
 
     it('should be rejected when not a function', function() {
-      expect(cms.addModule.bind(cms, 'html-content', null))
+      expect(frix.addModule.bind(frix, 'content', null))
         .to.throw('Not a function.');
     });
 
     it('should be rejected when event does not exist', function() {
-      expect(cms.addModule.bind(cms, 'invalid-string', someFunction))
+      expect(frix.addModule.bind(frix, 'invalid-string', someFunction))
         .to.throw('Event does not exist.');
     });
 
     it('should all be called', function(done) {
       let promises = [];
-      for (let [key,] of keva(cms.modules)) {
+      for (let [key,] of keva(frix.modules)) {
         promises.push(new Promise(resolve => {
-          cms.addModule(key, html => {
+          frix.addModule(key, html => {
             resolve(key);
             return html;
           });
